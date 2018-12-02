850b17e0eb35a6d0f75abb774e686f06b632ac6d
Marjorie Adolphi <marjorie.adolphi@epitech.eu>
2018-12-02 20:18:34 +0100

    rendu final

--- Maquette_Responsive.js
+++
@@ -1,5 +0,0 @@
-        /*<img src="7.jpg" alt="logo-facebook">
-        <img src="8.jpg" alt="logo-facebook">
-        <img src="9.jpg" alt="logo-facebook">
-        <img src="10.jpg" alt="logo-facebook">
-        <img src="11.jpg" alt="logo-facebook">*/
